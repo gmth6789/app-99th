0a464253eace80512b1934eed42a050ad07fcd77
gmth6789 <gmth.6789@gmail.com>
2023-08-30 06:47:42 +0700

    origin

--- client/frontstroe/src/App.js
+++ client/frontstroe/src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { Button1 } from "antd-mobile";
-import { Button2 } from 'antd';
-import { Button3 } from '@nutui/nutui-react';
-import Tabbar from './components/Tabbar';
+import Tabbar from './layouts/Tabbar';
 
 
 function App() {
   return (
     <div>
-    <h1>Hello Antd Mobile</h1>
-    <Button1>123</Button1>
 <Tabbar />
     </div>
   );
